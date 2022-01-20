0460978cd899b23142c1d4ac9ad270e3c1e6f46d
MagdK <kovacsmagda@gmail.com>
2022-01-20 13:33:57 +0100

    Added data

--- frontend/script.js
+++ frontend/script.js
@@ -1,13 +1,54 @@
+/* const data = [
+    "Gergely Kiss",
+    "Krisztian Bui",
+    "Bucko Balazs",
+    "Andras Varga",
+    "Borbala Terez Kovacs"
+];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favoriteFood: "csirke/rizs"
+    },
+    {
+        name: "Andras Varga",
+        favoriteFood: "brassoi"
+    },
+    {
+        name: "Balazs Bucko",
+        favoriteFood: "kolbaszos rajas pizza"
+    },
+    {
+        name: "Krisztian Bui",
+        favoriteFood: "banan"
+    },
+    {
+        name: "Borbala Terez Kovacs",
+        favoriteFood: "avokado"
+    },
+    {
+        name: "Benett Viszokai",
+        favoriteFood: "alma"
+    },
+    {
+        name: "Krisztian Porneszi",
+        favoriteFood: "almaspite"
+    },
+]
 function loadEvent() {
     console.log("My page is loaded")
+    
+    const root = document.querySelector("#root")
 
-    const list = document.querySelectorAll("section")
-
-    for (const elem of list) {
-        elem.insertAdjacentHTML("beforeend",`
-        <p>text</p>
-        `)  
-        //elem.querySelector("h1").outerHTML = "" 
+    for (const frontendStudent of data) {
+       root.insertAdjacentHTML("beforeend", `
+            <section>
+                <h1>${frontendStudent.name}</h1>
+                <p>${frontendStudent.favoriteFood}</p>
+            </section>
+        `) 
     }
 }
 window.addEventListener("load", loadEvent )
